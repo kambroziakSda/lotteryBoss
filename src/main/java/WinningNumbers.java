9650691c25e39a89e12989d883fa88402ac8a8c9
Krzysztof Ambroziak <ka.ambroziak@gmail.com>
2017-11-18 10:37:09 +0100

    LotteryBoss template

--- src/main/java/WinningNumbers.java
+++ src/main/java/WinningNumbers.java
@@ -1,24 +1,14 @@
-import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.util.stream.Collectors;
 
 /**
  * Created by krzysztof on 12.11.17.
  */
-@Path("/numbers")
 public class WinningNumbers {
-
-    @Inject
-    private NumberGenerator numberGenerator;
 
     @GET
     public Response getWinningNumbers() {
-        String winningNumbers = numberGenerator.getWinningNumbers()
-                .stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(", "));
+        String winningNumbers = "";
 
         return Response
                 .ok(winningNumbers)
