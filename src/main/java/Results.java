7816d5aa98c2dd7f78539cb73af4afeb1ea455d3
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2018-08-04 21:33:22 +0200

    java11

--- src/main/java/Results.java
+++ src/main/java/Results.java
@@ -2,6 +2,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -17,6 +18,7 @@ public class Results {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN+";charest=utf8")
     public Response getLotteryResults(LotteryBossParameters lotteryBossParameters) {
         List<Integer> winningNumbers = numberGenerator.getWinningNumbers();
         boolean isSuccess = isSuccess(lotteryBossParameters.getRandomNumbers(), winningNumbers, lotteryBossParameters.getLevel());
