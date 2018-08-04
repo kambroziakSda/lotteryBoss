5681126ba7e4e7956c0610296463eead8a1141a2
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2018-08-04 21:32:02 +0200

    java11

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -1,11 +1,19 @@
 import org.apache.log4j.Logger;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * Created by krzysztof on 12.11.17.
  */
+@Singleton
+@Startup
 public class NumberGenerator {
 
     private static final Logger logger = Logger.getLogger(NumberGenerator.class);
@@ -14,16 +22,23 @@ public class NumberGenerator {
 
     /**
      * W implementacji tej metody można posłużyć się klasą java.util.Random
+     *
      * @return
      */
     private List<Integer> drawWinningNumbers() {
-        return Collections.emptyList();
+        return new Random().ints(6, 1, 50)
+                .mapToObj(Integer::valueOf)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
     }
 
+    @PostConstruct
     public void generateNumbersOnStart() {
-
+        winningNumbers = drawWinningNumbers();
+        logger.info("Liczby wygrywające: "+winningNumbers);
     }
 
-
-
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
 }
--- src/main/java/Results.java
+++ src/main/java/Results.java
@@ -1,25 +1,55 @@
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
  * Created by krzysztof on 12.11.17.
  */
+@Path("/results")
 public class Results {
 
-    public Response getLotteryResults(List<Integer> userNumbers) {
+    @Inject
+    private NumberGenerator numberGenerator;
 
-        return null;
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getLotteryResults(LotteryBossParameters lotteryBossParameters) {
+        List<Integer> winningNumbers = numberGenerator.getWinningNumbers();
+        boolean isSuccess = isSuccess(lotteryBossParameters.getRandomNumbers(), winningNumbers, lotteryBossParameters.getLevel());
+        StringBuilder responseBuilder = new StringBuilder("Liczby wygrywajace to: " + winningNumbers);
+        if (isSuccess) {
+            responseBuilder.append("Brawo! Wygrałeś milion!");
+        } else {
+            responseBuilder.append("Niestety nie wygrałeś, spróbuj ponownie");
+        }
+        return Response.ok(responseBuilder.toString()).build();
     }
 
     /**
      * Implmentację tej metody można oprzeć na zwykłej pętli, lub API JAVA8 (stream, filter, collect, Collectors, toList())
      * W obu przypadkach przyda się metoda contains z interfejsu List
+     *
      * @param numbers
      * @param winningNumbers
+     * @param level
      * @return
      */
-    private boolean isSuccess(List<Integer> numbers, List<Integer> winningNumbers) {
-        return false;
+    private boolean isSuccess(List<Integer> numbers, List<Integer> winningNumbers, Integer level) {
+        int counter = 0;
+        for (Integer winningNumber : winningNumbers) {
+            if (numbers.contains(winningNumber)) {
+                counter++;
+            }
+        }
+        return counter >= level;
+
+/*        return winningNumbers.stream()
+                .filter(winningNumber -> numbers.contains(winningNumber))
+                .count() > 0;*/
     }
 
 }
